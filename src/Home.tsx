3fc1469158e2f50bea0dbe40357608aa1b7d0975
Grace-Shao <gshao8@gatech.edu>
2024-02-19 23:24:23 -0500

    landing page logo + buttons

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,13 @@
+import logoBlack from '../public/logoBlack.png';
+import './styles/homepage.css'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="center">
+      <img src={logoBlack} alt="Logo" className="logo" />
+      <h2>Renting made simple, for students, by students</h2>
+      <button>Login</button>
+      <button>Sign Up</button>
     </div>
   );
 };
