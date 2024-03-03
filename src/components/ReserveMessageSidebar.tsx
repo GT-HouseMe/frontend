765ea264740db306305ebf4f59c69510e0e17b96
Grace-Shao <gshao8@gatech.edu>
2024-03-02 19:28:02 -0500

    styling, now a reserve message sidebar

---
+++ src/components/ReserveMessageSidebar.tsx
@@ -0,0 +1,11 @@
+import "../styles/ReserveMessageSidebar.css";
+const ReserveMessageSidebar = () => {
+    //const { reserveMessage } = useReserveMessage();
+    return (
+        <div className="outline">
+        <h2>Reserve Message</h2>
+        <button>Reserve</button>
+        </div>
+    );
+}
+export default ReserveMessageSidebar;
