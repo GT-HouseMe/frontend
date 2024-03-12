7fab426869472833e96d4586514e42bcce9967cd
dbaglodi <baglodidisha@gmail.com>
2024-03-12 19:15:25 -0400

    added signup route

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import Home from "./Home.tsx";
 import Listings from "./routes/Listings.tsx";
 import Profile from "./routes/Profile.tsx";
 import Login from "./routes/Login.tsx"
+import SignUp from "./routes/SignUp.tsx"
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />
+      },
+      {
+        path: "/signup",
+        element: <SignUp />
       }
     ],
   },
