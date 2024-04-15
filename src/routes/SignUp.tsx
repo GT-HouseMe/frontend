e10507badc9b2f99a2672885e4381248a0302f9c
Amy <45153739+amtang325@users.noreply.github.com>
2024-04-15 12:24:38 -0400

    added register functionality

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,17 +1,50 @@
 import { useRef } from 'react'
-
+import axios from 'axios'
 import "../styles/base.css"
 //import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+  const nameRef = useRef<HTMLInputElement>(null)
   const emailRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
+  const descriptionRef = useRef<HTMLInputElement>(null)
+  const navigate = useNavigate();
 
-  const handleFormSubmit = (e: React.FormEvent) => {
+  const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    
-    if (emailRef.current && passwordRef.current) {
-      console.log(emailRef.current.value, passwordRef.current.value)
+
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const description = descriptionRef.current?.value;
+
+    if (nameRef.current && emailRef.current && passwordRef.current && descriptionRef.current) {
+      const userData = await signUpUser({
+        name,
+        email,
+        password,
+        description
+      })
+    }
+  }
+
+  async function signUpUser(credentials) {
+    try {
+      const response = await axios.post('http://localhost:3000/auth/register', credentials, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const userData = response.data.token;
+      console.log('User data:', userData);
+      Cookies.set('userData', userData, { expires: 7 });  //expires in 7 days
+      navigate('/listings');
+      return userData;
+    } catch (error) {
+      console.error('An error occurred:', error);
+      throw error; // Propagate the error for handling further up the chain if needed
     }
   }
 
@@ -20,6 +53,13 @@ const SignUp = () => {
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" onSubmit={handleFormSubmit}>
+              <div>
+                  <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                    <div className="mt-2">
+                    <input ref={nameRef} id="name" name="name" autoComplete="name" required className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+              </div>
+
               <div>
                   <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                     <div className="mt-2">
@@ -38,10 +78,10 @@ const SignUp = () => {
 
               <div>
                   <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Re-enter Password</label>
+                    <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">Description</label>
                   </div>
                   <div className="mt-2">
-                    <input ref={passwordRef} id="password" name="password" type="password" autoComplete="current-password" required className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input ref={descriptionRef} id="description" name="description" type="textarea" autoComplete="description" required className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   </div>
               </div>
 
